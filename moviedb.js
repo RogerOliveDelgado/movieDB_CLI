2ff22e8ecfcd3a87980d63cc106ff520ef3bbefe
Alicia Cembranos <alicembru15@gmail.com>
2022-09-05 15:05:48 +0200

    Add main settings of commander to main file

--- moviedb.js
+++ moviedb.js
@@ -28,3 +28,37 @@ program
   .option("--popular", "Display the most popular persons");
 
 program.parse();
+
+
+const { Command } = require("commander");
+const program = new Command();
+const dotenv = require("dotenv");
+const ora = require("ora");
+
+dotenv.config();
+
+//Command settings
+program.name("moviedb").description("CLI to MovieDB").version("0.0.1");
+
+//Set help setting
+program.configureHelp({
+  sortSubcommands: true,
+  subcommandTerm: (cmd) => cmd.name(),
+});
+
+//Get persons command
+program
+  .name("get-persons")
+  .description("Make a network request to fetch the most popular persons")
+  .requiredOption("-p, --popular", "Fetch the popular persons")
+  .requiredOption(
+    "--page <number>",
+    "The page of persons data results to fetch"
+  )
+  .option("-s, --save", "Store the data in the local file system.")
+  .option("-l, --local", "Read the data from the local file system")
+  .action(() => {
+    console.log("options", program.opts());
+  });
+
+program.parse(process.argv);
